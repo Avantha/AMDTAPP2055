33760e0e26f8ed513165abf1c3dd9e5c784ca897
Avantha <avanthamedis1@gmail.com>
2020-10-08 19:58:06 +0530

    Added covidCheck

---
+++ covidCheck.js
@@ -0,0 +1,21 @@
+var readline = require('readline').createInterface ({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question('Enter your city', function (city){
+    if (city === "colombo") {
+        console.log("Low Risk Area");
+    }
+    else if (city === "galle") {
+        console.log("Low Risk Area")
+    }
+    else if (city === "gampaha") {
+        console.log("High Risk Area");
+    }
+    else {
+        console.log("City is incorrect");
+    }
+    console.log("Stay safe");
+    readline.close();
+});
